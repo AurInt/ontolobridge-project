1f2cb3e0dd1045cb7ad7116f51e1699dd4a12bea
KrishnaUdaiwal <Krishna.Udaiwal@cra-arc.gc.ca>
2024-03-15 21:04:35 -0400

    updated jwt token refresher with jjwt-api v.0.12.5 token builder removed app.jwtSecret as unneeded updated response refresh to add a header with authorization + bearer

--- src/main/java/edu/miami/schurer/ontolobridge/utilities/JWTRefreshInterceptor.java
+++ src/main/java/edu/miami/schurer/ontolobridge/utilities/JWTRefreshInterceptor.java
@@ -1,42 +1,42 @@
 package edu.miami.schurer.ontolobridge.utilities;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 
 @Component
 public class JWTRefreshInterceptor {
+
     private JwtProvider tokenProvider;
 
-    @Value("${app.jwtSecret}")
-    private String jwtSecret;
 
     @Value("${app.jwtExpiration}")
     private int jwtExpiration;
 
-    //cannot modify in post handle so do it here.
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//cannot modify in post handle so do it here.
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
 
         String jwt = getJwt(request);
         if (jwt != null && tokenProvider.validateJwtToken(jwt)) {
 
-            //TODO: Add in code to save to database to allow revokation
+            SecretKey key = Jwts.SIG.HS256.key().build();
+//TODO: Add in code to save to database to allow revocation
             Date Expiration = tokenProvider.getExpirationFromJWTToken(jwt);
             if ((Expiration.getTime() - new Date().getTime()) < 87000) {
                 String token = Jwts.builder()
-                        .setSubject((tokenProvider.getUserNameFromJwtToken(jwt)))
-                        .setIssuedAt(new Date())
-                        .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-                        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                        .subject(tokenProvider.getUserNameFromJwtToken(jwt))
+                        .issuedAt(new Date())
+                        .expiration(new Date((new Date()).getTime() + jwtExpiration))
+                        .signWith(key)
                         .compact();
-                response.addHeader("jwtToken", token);
+                response.addHeader("Authorization", "Bearer " + token);
                 System.out.println("Refreshing Token");
             }
         }
@@ -53,3 +53,4 @@ public class JWTRefreshInterceptor {
         return null;
     }
 }
+
