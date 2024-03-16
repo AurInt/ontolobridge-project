5aabc5932c6adc7faa51aef6a5030efc4bda64c9
KrishnaUdaiwal <Krishna.Udaiwal@cra-arc.gc.ca>
2024-03-15 20:16:14 -0400

    Removed @Bean taskScheduler as not required with springboot version 3.2.x

--- src/main/java/edu/miami/schurer/ontolobridge/OntolobridgeApplication.java
+++ src/main/java/edu/miami/schurer/ontolobridge/OntolobridgeApplication.java
@@ -2,16 +2,11 @@ package edu.miami.schurer.ontolobridge;
 
 import edu.miami.schurer.ontolobridge.utilities.JWTRefreshInterceptor;
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.handler.MappedInterceptor;
 
@@ -25,11 +20,6 @@ public class OntolobridgeApplication {
     JWTRefreshInterceptor interceptor;
     public static void main(String[] args) {
         SpringApplication.run(OntolobridgeApplication.class, args);
-    }
-
-    @Bean
-    public TaskScheduler taskScheduler() {
-        return new ConcurrentTaskScheduler(); //single threaded by default
     }
 
     @Bean
